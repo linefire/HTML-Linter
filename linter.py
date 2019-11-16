1c37ad952160af357c3dea4ad1a4a223c3683163
LineFire <sasha.lada5@gmail.com>
2019-11-16 16:31:17 +0300

    Добавив головне меню утиліти

--- linter.py
+++ linter.py
@@ -2,7 +2,9 @@
 
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
+
+from os import system
 
 
 class Linter:
@@ -10,5 +12,34 @@ class Linter:
     
     Клас який керує шаблонами, настройками та форматуванням
     
+    Methods
+    -------
+    start_menu()
+        Відображає в консолі головне меню
     """
-    pass
+
+    def start_menu(self):
+        """Відображає в консолі головне меню"""
+        while True:
+            system("cls")
+            print("HTML Linter v{}".format(__version__))
+            print("0.Вихід")
+
+            try:
+                command = int(input("Введіть номер команди: "))
+            except ValueError:
+                print("Введіть НОМЕР команди.")
+                input("Натисніть Enter щоби продовжити.")
+                continue
+            
+            if command == 0:
+                exit()
+            else:
+                print("Невірна команда.")
+                input("Натисніть Enter щоби продовжити.")
+                continue
+            
+
+if __name__ == "__main__":
+    linter = Linter()
+    linter.start_menu()
